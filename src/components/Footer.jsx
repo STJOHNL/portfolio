8e6b79d6d7a3e7c56c3fc1e362c715880da87120
Logan St. John <loganwst.john@gmail.com>
2024-02-08 20:08:18 -0500

    Home page almost there

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,24 @@
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedinIn, faUpwork } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 
 
 const Footer = () => {
     return (
         <footer>
+            <h3>Get in touch</h3>
+            <Link>
+                <FontAwesomeIcon icon={faEnvelope} className="icon" />
+            </Link>
             <Link>
                 <FontAwesomeIcon icon={faLinkedinIn} className="icon" />
             </Link>
             <Link>
-                <FontAwesomeIcon icon={faGithub} className="icon" />
+                <FontAwesomeIcon icon={faUpwork} className="icon" />
             </Link>
             <Link>
-                <FontAwesomeIcon icon={faUpwork} className="icon" />
+                <FontAwesomeIcon icon={faGithub} className="icon" />
             </Link>
         </footer>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,44 +1,75 @@
 import { Link } from "react-router-dom"
+import LogoCarousel from "../components/LogoCarousel"
+import loganLego from '../assets/images/loganLego.gif'
 
 const Home = () => {
     return (
         <>
             <section className="hero-section">
-                <h1>I'm Logan.</h1>
-                <h2>Software Developer</h2>
-                <img src="https://placekitten.com/200/300" alt="" />
+                <div className="hero-title">
+                    <h1>Logan St. John</h1>
+                    <h2>Software Developer</h2>
+                </div>
+                <div className="hero-cta">
+                    <div className="absolute-container">
+                        <div className="hero-btn-container glass">
+                            <Link
+                                to="/"
+                                className="btn"
+                            >About</Link>
+                            <Link
+                                to="/"
+                                className="btn"
+                            >Contact</Link>
+                        </div>
+                    </div>
+                </div>
             </section>
-            <section className="about-section">
-                <h3>About</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae facere atque amet voluptatem, minima ipsum beatae incidunt, maiores vero pariatur hic aliquid fugiat perferendis, impedit numquam dolor distinctio.</p>
-                <Link>Learn more</Link>
+            <section className="client-slider">
+                <h3>I'm grateful to have worked with such clients</h3>
+                <LogoCarousel />
             </section>
             <section className="portfolio-section">
-                <h3>My work</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae facere atque amet voluptatem, minima ipsum beatae incidunt, maiores vero pariatur hic aliquid fugiat perferendis, impedit numquam dolor distinctio.</p>
+                <hr />
+                <div className="title-blurb">
+                    <h3>My work</h3>
+                    <p>Explore a selection of client and personal projects I've developed.</p>
+                </div>
                 <section className="portfolio-cards">
-                    <div className="portfolio-card">
-                        <img src="https://placekitten.com/300/300" alt="" />
+                    <div className="portfolio-card glass">
                         <h5>Project Name</h5>
+                        <img src="https://placekitten.com/300/300" alt="" />
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero veniam exercitationem doloribus minus quasi aut!</p>
                     </div>
-                    <div className="portfolio-card">
-                        <img src="https://placekitten.com/300/300" alt="" />
+                    <div className="portfolio-card glass">
                         <h5>Project Name</h5>
+                        <img src="https://placekitten.com/300/300" alt="" />
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero veniam exercitationem doloribus minus quasi aut!</p>
                     </div>
-                    <div className="portfolio-card">
-                        <img src="https://placekitten.com/300/300" alt="" />
+                    <div className="portfolio-card glass">
                         <h5>Project Name</h5>
+                        <img src="https://placekitten.com/300/300" alt="" />
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero veniam exercitationem doloribus minus quasi aut!</p>
                     </div>
                 </section>
-                <Link>Portfolio</Link>
+                <Link
+                    to='/portfolio'
+                    className="btn"
+                >View more projects</Link>
             </section>
-            <section className="contact-section">
-                <h3>Get in touch</h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae facere atque amet voluptatem, minima ipsum beatae incidunt, maiores vero pariatur hic aliquid fugiat perferendis, impedit numquam dolor distinctio.</p>
-                <Link>Contact me</Link>
+            <section className="about-section">
+                <img
+                    src={loganLego}
+                    alt="Logan with a Lego statue"
+                />
+                <div>
+                    <h3>About</h3>
+                    <p>Skilled Full Stack Developer with expertise in JavaScript, Node.js, MongoDB, and React, delivering dynamic web solutions that boost business growth.</p>
+                    <Link
+                        to='/about'
+                        className="btn"
+                    >Learn more</Link>
+                </div>
             </section>
         </>
     )
