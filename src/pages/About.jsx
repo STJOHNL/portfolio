515e9a28c7ea49bd12ae099e38d09816d324e63a
Logan <loganwst.john@gmail.com>
2024-02-18 15:35:33 -0500

    about edit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,9 +14,26 @@ const About = () => {
             </ul>
 
             <h2>Skills & Certifications</h2>
-            <ul>
-                <li></li>
-            </ul>
+            <div className="skills-container">
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>JavaScript</p>
+                <p>React.js</p>
+                <p>Node.js</p>
+                <p>Express.js</p>
+                <p>MongoDB</p>
+                <p>Figma</p>
+                <p>Google Suite</p>
+                <p>Elementor</p>
+                <p>Divi</p>
+                <p>Wordpress</p>
+                <p>Mailchimp</p>
+                <p>Blueshift</p>
+                <p>Zaius</p>
+                <p>Hubspot</p>
+                <p>SendGrid</p>
+                <p>JotForm</p>
+            </div>
         </div>
     )
 }
